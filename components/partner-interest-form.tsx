bb9eac7991d6562e7138571edc287e82afadbad0
Greg Giesecke <info@electricspinz.com>
2026-08-11 19:38:46 -0400

    fix: correct partner form React reference

--- components/partner-interest-form.tsx
+++ components/partner-interest-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { type FormEvent, type ReactNode, useEffect, useRef, useState } from "react";
+import React, { type FormEvent, type ReactNode, useEffect, useRef, useState } from "react";
 import { Button } from "@/components/button";
 import { trackPartnerInterestStarted, trackPartnerInterestSubmitted, trackPartnerPageView } from "@/lib/analytics";
 
